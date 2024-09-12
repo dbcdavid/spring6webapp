fcda4067866a425381f0009fca4339e51d77d815
David <dbcdavid.clk@gmail.com>
2024-09-12 13:36:20 -0300

    spring 6 web app first commit

---
+++ src/test/java/classes/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package classes.guru.springframework.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
